132f4316cb6fd33bc0aa718a5820d7bd34aefe91
chenhaifeng <1397760231@qq.com>
2017-11-13 23:12:41 +0800

    bug修复

--- TrainForStudents/LoginViewController.swift
+++ TrainForStudents/LoginViewController.swift
@@ -200,6 +200,7 @@ class LoginViewController : MyBaseUIViewController, UIPickerViewDataSource , UIP
                     let token = json["token"].stringValue
                     r_token = token
                     UserDefaults.standard.set(token, forKey: LoginInfo.token.rawValue)
+                    UserDefaults.standard.set(self.txt_loginId.text!, forKey: LoginInfo.loginId.rawValue)
                     //注册极光推送别名
                     JPUSHService.setAlias(json["userkey"].stringValue, completion: nil, seq: 0)
                     print("极光推送注册的别名:\(json["userkey"].stringValue)")
